75bb715163d958a1800fb9e1136c0302613082fb
Viacheslav Lysov <entsllor@gmail.com>
2023-11-18 19:59:09 +0600

    feat: update is-iso-date-string logic

--- src/helpers/datetimes/is-iso-date-string.ts
+++ src/helpers/datetimes/is-iso-date-string.ts
@@ -3,5 +3,5 @@ export function isIsoDateString(value: unknown): value is string {
         return false
     }
     const date = new Date(value);
-    return !isNaN(date.getTime()) && date.toISOString() === value.toString();
+    return !isNaN(date.getTime()) && /^-?\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(value);
 }
